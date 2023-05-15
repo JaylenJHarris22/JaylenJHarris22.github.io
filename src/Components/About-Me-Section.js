3c5b6c9e1ab141eec30e0ffeb3b2c854e9ffe1cb
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-05-14 22:30:01 -0500

    added the projects and the resume components

--- src/Components/About-Me-Section.js
+++ src/Components/About-Me-Section.js
@@ -2,7 +2,7 @@ export default function AboutMeSection (){
 return(
     <div className='w-full bg-black min-h-screen text-white grid grid-cols-1 md:grid-cols-5'>
         <div className='col-span-2'></div>
-        <div className='col-span-3 flex flex-col content-center text-gre'>
+        <div className='col-span-3 flex flex-col content-center'>
             <h3 className='text-green-400 text-2xl font-semibold'>About Me</h3>
             <p>Hello I'm Jaylen, a software engineer that has a passion for building applications and developing modern solutions. I am a thinker and an innovator looking to bring my personal introspection into the world.</p>
         </div>
---
+++ src/Components/Resume.js
@@ -0,0 +1,14 @@
+export default function Resume () {
+    return(
+        <div>
+            <div>
+                <h3>Skills</h3>
+                <p></p>
+            </div>
+            <div>
+                <h3>Skills</h3>
+                <p></p>
+            </div>
+        </div>
+    )
+}
