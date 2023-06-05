1b0e9a49f7cb01df25f91bd0f01d7b6a25629d7a
Jaylen <harris.j.jaylen22@gmail.com>
2023-06-04 22:39:39 -0500

    added in grid and some styling

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,11 @@
 export default function Projects () {
     return (
         <div className='min-h-screen w-full bg-dark-gray '>
-            <div></div>
+            <div className='grid grid-cols-3 mx-auto w-4/5 justify-items-center'>
+                <div className='bg-white w-4 h-3'></div>
+                <div className='bg-white w-4 h-3'></div>
+                <div className='bg-white w-4 h-3'></div>
+            </div>
             <div></div>
         </div>
     )
